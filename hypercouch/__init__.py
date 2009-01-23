61ff2163869eec2a680f2b35d0e015a1428828cf
davisp <davisp@davis2318.l3u.neb.com>
2009-01-23 15:33:04 -0500

    Things appear to be working fairly well.

--- hypercouch/__init__.py
+++ hypercouch/__init__.py
@@ -1,6 +1,13 @@
+"""\
+Copyright (c) 2009 Paul J. Davis <paul.joseph.davis@gmail.com>
+This file is part of hypercouch which is released uner the MIT license.
+"""
+
 from __future__ import with_statement
 
 import optparse as op
+import os
+import sys
 
 import simplejson
 
@@ -8,7 +15,7 @@ import hypercouch.index
 
 def options():
     return [
-        op.make_option("-d", "--dir", dest="dir", default="./",
+        op.make_option("-d", "--dir", dest="dir", default="/tmp/hypercouch",
             help = "Directory to store databases in. [%default]"),
         op.make_option("-u", "--uri", dest="uri", default="http://127.0.0.1:5984",
             help = "CouchDB Server URI [%default]"),
@@ -28,8 +35,14 @@ def main():
         parser.print_help()
         exit(-1)
     idx = hypercouch.index.Index(opts.dir, opts.uri)
-    for req in requests():
-        idx.query(req)
+    try:
+        for req in requests():
+            resp = idx.query(req)
+            sys.stdout.write("%s\n" % simplejson.dumps(resp))
+            sys.stdout.flush()
+    finally:
+        idx.close()
+        os._exit(-1)
 
 if __name__ == '__main__':
     main()
--- hypercouch/index.py
+++ hypercouch/index.py
@@ -1,8 +1,15 @@
+"""\
+Copyright (c) 2009 Paul J. Davis <paul.joseph.davis@gmail.com>
+This file is part of hypercouch which is released uner the MIT license.
+"""
+
 from __future__ import with_statement
 
 import os
 import Queue
+import sys
 import threading
+import traceback
 
 import couchdb
 import hypy
@@ -12,6 +19,8 @@ import spidermonkey
 class Index(object):
     def __init__(self, idxdir, uri):
         self.idxdir = idxdir
+        if not os.path.exists(idxdir):
+            os.mkdir(idxdir)
         self.hdb = hypy.HDatabase()
         self.hdb.open(self.idxdir, "a")
         self.couch = couchdb.Server(uri)
@@ -20,18 +29,30 @@ class Index(object):
         self.cx.bind_callable("index", self.add_text)
         self.cx.bind_callable("property", self.add_attribute)
         self.lock = threading.RLock()
+        self.comitted = False
+        self.closing = False
         self.queue = Queue.Queue()
         self.state = {}
         self.values = []
         self.curr_doc = None
         self.indexer = threading.Thread(target=self.index)
+        self.indexer.setDaemon(False)
         self.indexer.start()
+
+    def close(self):
+        with self.lock:
+            self.closing = True
+            self.hdb.close()
+        self.indexer.join(5)
 
     def query(self, req):
         self.queue.put((req["info"]["db_name"], req["info"]["update_seq"]))
         (cond, highlight) = self.build(req)
         with self.lock:
-            res = self.db.search(cond)
+            if self.comitted:
+                self.hdb.open(self.idxdir, "a")
+                self.comitted = False
+            res = self.hdb.search(cond)
         rows = []
         for doc in res:
             h = {"id": doc[u"@uri"]}
@@ -40,8 +61,8 @@ class Index(object):
             for key in doc.keys():
                 if key[:1] == "@":
                     continue
-                res[key] = doc[key]
-            ret.append(res)
+                h[key] = doc[key]
+            rows.append(h)
         return {"code": 200, "json": {"rows": rows}}
 
     def build(self, req):
@@ -66,63 +87,87 @@ class Index(object):
         return (ret, highlight)
 
     def index(self):
-        self.read_state()
-        while True:
-            (dbname, update_seq) = self.queue.get()
-            (funcs, curr_seq) = self.prepare(dbname)
-            
-            if len(funcs) < 1:
-                continue
-            elif update_seq < curr_seq:
-                self.reset_db(dbname)
-            elif update_seq == curr_seq:
-                return
-            # else update required
-
-            updates = cdb.view("_all_docs_by_seq", startkey=curr_seq, limit=500)
-            while len(updates) > 0:
-                docids = []
-                processed = []
-                for row in updates.rows:
-                    if row.id.startswith("_design/"):
-                        self.purge(dbname)
-                        return
-                    docids.append(row.id)
-                for row in cdb.view("_all_docs", keys=docids, include_docs=True):
-                    self.curr_doc = hypy.HDocument(uri=unicode(row.doc["_id"]))
-                    for fn in funcs:
-                        fn(row.doc)
-                    self.processed.append(self.curr_doc)
+        try:
+            self.read_state()
+            while True:
                 with self.lock:
-                    for doc in processed:
-                        self.hdb.putDoc(doc)
-                    self.hdb.flush()
-                curr_seq = updates.rows[-1]["key"]
-                updates = cdb.view("_all_docs_by_seq", startkey=update_seq, limit=500)
-            self.write_state(dbname, update_seq)
+                    if self.closing:
+                        return
+                try:
+                    (dbname, update_seq) = self.queue.get(True, 10)
+                except Queue.Empty:
+                    continue
+                db = self.couch[dbname]
+                (funcs, curr_seq) = self.prepare(db, dbname)
+            
+                if len(funcs) < 1:
+                    continue
+                elif update_seq < curr_seq:
+                    self.purge(dbname)
+                    continue
+                elif update_seq == curr_seq:
+                    continue
+                # else update required
 
-    def prepare(self, dbname):
+                updates = db.view("_all_docs_by_seq", startkey=curr_seq, limit=500)
+                while len(updates) > 0:
+                    docids = []
+                    processed = []
+                    for row in updates.rows:
+                        docids.append(row.id)
+                    for row in db.view("_all_docs", keys=docids, include_docs=True):
+                        if row.id.startswith("_design/"):
+                            if "ft_indx" not in row.doc:
+                                continue
+                            if row.id not in funcs or row.doc["ft_index"] != funcs[row.id]:
+                                self.purge(dbname)
+                            continue
+                        self.curr_doc = hypy.HDocument(uri=unicode(row.doc["_id"]))
+                        for ddoc, fn in funcs.iteritems():
+                            fn(row.doc)
+                        processed.append(self.curr_doc)
+                    curr_seq = updates.rows[-1]["key"]
+                    with self.lock:
+                        if self.closing:
+                            return
+                        for doc in processed:
+                            self.hdb.putDoc(doc)
+                        self.hdb.flush()
+                        self.state[dbname]["update_seq"] = curr_seq
+                        self.write_state()
+                        self.comitted = True
+                    updates = db.view("_all_docs_by_seq", startkey=curr_seq, limit=500)
+        # Indexing thread exiting.
+        except Exception, inst:
+            sys.stderr.write("Uncaught exception in indexing thread, shutting down forcefully.\n")
+            traceback.print_exc()
+            os._exit(-1)
+
+    def prepare(self, db, dbname):
         funcs = {}
         update_seq = 0
-        with self.lock:
-            if "functions" in self.state.get(dbname, {}):
-                return self.state[dbname]["functions"]
-            for row in self.cdb.view("_all_docs", startkey="_deisgn/", endkey="_design0", include_docs=True):
-                if "ft_index" in row.doc:
-                    funcs[row.id] = self.cx.eval_script(row.doc["ft_index"])
-            self.state.setdefault(dbname, {})
-            self.state[dbname]["functions"] = funcs
-            self.state[dbname].setdefault("update_seq", 0)
-            sig = self.state[dbname]["update_seq"]
+        if "functions" in self.state.get(dbname, {}):
+            for k, v in self.state[dbname]["functions"].iteritems():
+                funcs[k] = self.cx.eval_script(v)
+            return (funcs, self.state[dbname]["update_seq"])
+        self.state.setdefault(dbname, {})
+        self.state[dbname].setdefault("update_seq", 0)
+        self.state[dbname].setdefault("functions", {})
+        for row in db.view("_all_docs", startkey="_deisgn/", endkey="_design0", include_docs=True):
+            if "ft_index" in row.doc:
+                self.state[dbname]["functions"][row.id] = row.doc["ft_index"]
+                funcs[row.id] = self.cx.eval_script(row.doc["ft_index"])
+        sig = self.state[dbname]["update_seq"]
         return (funcs, update_seq)
 
     def purge(self, dbname):
         cond = hypy.HCondition(u"*", matching="simple")
-        cond.addAttr("@dbname STREQ %s" % dbname)
-        with self.lock:
-            for row in self.db.search(cond):
-                self.db.remove(row)
-            self.db.flush()
+        cond.addAttr(u"@dbname STREQ %s" % dbname)
+        for row in self.hdb.search(cond):
+            self.hdb.remove(row)
+        self.hdb.flush()
+        del self.state[dbname]
+        self.write_state()
 
     def read_state(self):
         fname = os.path.join(self.idxdir, "couchdb.state")
