ec34484e7594c0820f090380ca60056b31a8c87b
davisp <davisp@davis2318.hsd1.ma.comcast.net>
2009-01-23 23:44:47 -0500

    Added license/copyright notices.

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""\
+Copyright (c) 2009 Paul J. Davis <paul.joseph.davis@gmail.com>
+This file is part of hypercouch which is released uner the MIT license.
+"""
+
--- tests/order_test.py
+++ tests/order_test.py
@@ -1,3 +1,7 @@
+"""\
+Copyright (c) 2009 Paul J. Davis <paul.joseph.davis@gmail.com>
+This file is part of hypercouch which is released uner the MIT license.
+"""
 import time
 import unittest
 import couchdb
